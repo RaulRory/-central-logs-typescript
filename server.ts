7f25471862d17bd89d77d0c45ebcb95320620acf
Raul Rory <raul.pereira@kroton.com.br>
2021-12-28 23:06:11 -0300

    Creating file server.ts

---
+++ server.ts
@@ -0,0 +1,7 @@
+import { createServer, Server } from "http";
+
+const server: Server =  createServer();
+
+server.listen(3000, () => {
+    console.log('Server TypeScipt running in port 3000');
+});
