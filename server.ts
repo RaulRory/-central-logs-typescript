d859854a945d1140e4df459a9873acd94d0d7230
Raul Rory <raul.pereira@kroton.com.br>
2022-01-03 00:51:24 -0300

    test huhky command

--- server.ts
+++ server.ts
@@ -3,5 +3,5 @@ import { createServer, Server } from "http";
 const server: Server =  createServer();
 
 server.listen(3000, () => {
-    console.log('Server TypeScipt running in port 3000');
-});
+console.log('Server TypeScipt running in port 3000')
+})
