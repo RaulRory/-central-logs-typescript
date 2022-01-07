a58581d24e638a0f113225c2a54d68b000d80062
Raul Rory <raul.pereira@kroton.com.br>
2022-01-06 22:09:58 -0300

    creating file jest config and test app Express

---
+++ jest.config.js
@@ -0,0 +1,6 @@
+/** @type {import('ts-jest/dist/types').InitialOptionsTsJest} */
+module.exports = {
+  modulePaths: ['<rootDir>'],
+  preset: 'ts-jest',
+  testEnvironment: 'node'
+};
---
+++ test/src/app.spec.ts
@@ -0,0 +1,20 @@
+import { createServer, Server } from 'http';
+import { app } from '../src/app';
+
+let server: Server;
+
+beforeEach((done) => {
+  server = createServer(app);
+  done();
+});
+
+afterEach(() => {
+  server.close();
+});
+
+describe('App Express', () => {
+  it('Server is Running', (done) => {
+    expect(server).toBeTruthy();
+    done();
+  });
+});
